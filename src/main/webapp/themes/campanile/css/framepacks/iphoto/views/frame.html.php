77821ab054c18cb5e7b7151ad20c86639fbc920a
Stefano Fornari <stefano.fornari@gmail.com>
2011-12-30 15:59:35 +0100

    moved content under campanile-core

--- src/main/webapp/themes/campanile/css/framepacks/iphoto/views/frame.html.php
+++
@@ -1,7 +0,0 @@
-<div class="g-thumb-header"><div class="g-thumb-corner"></div></div>
-  <div class="g-thumb-body">
-  	<div class="g-thumb-corner">
-  		<?= $thumb_content; ?>
-  	</div>
-  </div>
-<div class="g-thumb-footer"><div class="g-thumb-corner"></div></div>
